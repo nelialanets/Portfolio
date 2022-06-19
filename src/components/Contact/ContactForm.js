4a2def768d53480ec41dfba13994b338965f7a7c
nelialanets <neliarod@stanford.edu>
2022-06-18 20:16:27 -0700

    set up email form and react-mapp

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,41 +1,42 @@
 import React, { useRef } from "react";
-import { useEffect } from "react";
 import { useState } from "react";
-import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
 import './contact.scss'
 import emailjs from '@emailjs/browser';
 
+import { MapContainer, TileLayer, useMap, Popup,Marker } from 'react-leaflet'
 
 export default function ContactForm(){
 
-    const[letterClass, setLetterClass]=useState('text-animate')
-    const refForm=useRef()
 
+    const position = [51.505, -0.09]
+    const form=useRef()
+    
     const sendEmail=(e)=>{
-        e.preventDefault()
+        e.preventDefault();
 
-        emailjs
-        .sendForm(
-            'gmail',
-            'template_toxouiq',//template id
-            refForm.current,
-            'service_ks7rxm4'
+        emailjs.sendForm(
+            // 'gmail',
+            'service_ks7rxm4',
+            'template_g3hmq0i',
+
+            form.current,
+            '6BsU-Z3YKdfbPQlLK'
         )
-        .then(()=>{
-            alert ('Email was successfully sent')
+        .then(
+            (result)=>{
+             alert ('Email was successfully sent')
             window.location.reload(false)
+            console.log(result.text);
         },
-        ()=>{
+        (error)=>{
+
             alert ('Email was not successfully sent please try again')
+            console.log(error.text)
+           
         }
         )
     }
 
-    // useEffect(()=>{
-    //     return setTimeout(() => {
-    //         setLetterClass('text-aimate-hover')
-    //     }, 4000)
-    // },[])
 
     return(
         
@@ -46,36 +47,53 @@ export default function ContactForm(){
             <div className="text-zone">
             <h1 className="contact-me-h1">Contact Me </h1>
             <div className="contact-form">
-                <form ref={refForm} onSubmit={sendEmail}>
+                <form ref={form} onSubmit={sendEmail}>
                     <ul>
                         <li className="half">
                             <input type="text"
-                             name="name"
-                              placeholder="Enter your name" 
-                              required />
-                        </li>
-                        <li className="half">
-                            <input type="email"
-                             name="email" 
-                             placeholder="Enter your email " 
+                             name="user_name"
                              required />
-                        </li>
+                        </li> 
+                         <li className="half">
+                            <input type="email"
+                             name="user_email" 
+                             required
+                              />
+                         </li> 
                         <li >
                            <textarea
-                           placeholder="Enter your message"
+                           
                            name="message"
+                           placeholder="Enter your message"
                            required
                            />  
-                        </li>
-                        <li>
-                            <input type="submit" className="flat-btn2" value='send' />
-                        </li>
-                    </ul>
+                         </li> 
+                         <li>
+                            <input type="submit" className="flat-btn2" value='Send' />
+                         </li> 
+                     </ul>
                 </form>
-               </div>
-            </div>
-        </div>
+           </div>
+         </div>
+         <div className="info-map">
+            NeliaDev
 
-        </div>
+            <br />
+            Palo Alto, CA
+            <br />
+            <span>nelialanets@gmail.com</span>
+         </div>
+         <div className="map-wrap">
+         <MapContainer center={[44.96366, 19.61045]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={position}>
+              <Popup>Sloba lives here, come over for a cup of coffee</Popup>
+            </Marker>
+          </MapContainer> 
+         </div>
+     </div>
+     
+     </div>
     )
 }
