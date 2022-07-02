4f43d94c125d44a6cf61741b12a4fae489f877db
nelialanets <neliarod@stanford.edu>
2022-07-02 13:25:10 -0700

    clean up3

--- src/components/Portfolio/PortfolioItems.js
+++ src/components/Portfolio/PortfolioItems.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './index.scss'
 import Data from "../../components/data/portfolio.json"
-import AnimatedLetters from "../AnimatedLetters/AnimatedLetters"
 
 
 export default function PortfolioItems() {
